045ff4d5f4dc220e75a570920e09c27df712ff70
jsquared21 <jabarimail@gmail.com>
2017-01-17 22:13:44 -0500

    Add ex2

--- ex2.py
+++ ex2.py
@@ -1,8 +1,9 @@
-print "Hello World"
-# print "Hello Again"
-# print "I like typing this."
-# print "this is fun."
-# print 'Yay! Printing.'
-# print "I'd much rather you  'not'."
-# print 'I "said" do not touch this.'
-# print "Printing another line."
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code
+# print "This won't run."
+
+print "This will run."
