f6fb8697fefcb5fe94ff0c70534d457e80b1e586
jsquared21 <jabarimail@gmail.com>
2017-01-19 17:26:13 -0500

    Add ex13

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# Impor the argv method from the sys module
+from sys import argv
+
+# Assign the arguement variables to the variables 
+# script, first, second, third
+script, first, second, third = argv
+
+# Display a string and the variable script
+print "The script is called:", script
+# Display a string and the variable first
+print "Your first variable is:", first
+# Display a string and the variable second
+print "Your second variable is:", second
+# Display a string and the variable third
+print "Your third variable is:", third
